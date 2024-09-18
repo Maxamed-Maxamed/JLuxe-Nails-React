fc8751d94abc4a4dd64ab648ce3c99d95bd6dde2
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-09-18 19:30:43 +0100

    Add NextUI plugin to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 // tailwind.config.js
+const { nextui } = require("@nextui-org/react");
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  // ...
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: "#0072f5",
+          }
+        },
+        dark: {
+          colors: {
+            primary: "#0072f5",
+          }
+        },
+      },
+    }),
+    ],
+};
